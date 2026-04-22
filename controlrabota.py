c2dd57c74ff1fe8377e8d4ab55b627396247c1fa
Datan <Datan.ctupide@yandex.ru>
2026-04-22 13:30:29 +0500

    Add files via upload

---
+++ controlrabota.py
@@ -0,0 +1,11 @@
+# part 2
+a = int(input())
+b = [2, 3, 2, 6, -2,]
+a6 = (a * 9/5) + 32
+print("6:", a6)
+if a >= 0:
+    print("8:", "polozhitelno")
+else:
+    print("8:", "otrizatelno")
+a9 = 2 + 4 + 6 + 8
+print("9:", a9)
